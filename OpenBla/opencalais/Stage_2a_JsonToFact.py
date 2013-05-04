4597950820a5f6eb61efc6935f82d07c59e1d089
Peter Büchler <p.buechler@hotmail.com>
2013-05-04 20:06:38 +0200

    alles neu unso

--- OpenBla/opencalais/Stage_2a_JsonToFact.py
+++ OpenBla/opencalais/Stage_2a_JsonToFact.py
@@ -4,25 +4,7 @@ Created on 04.05.2013
 @author: Peter
 '''
 import json
-class Fact(object):
-    
-    def __init__(self,factType,factTypeGroup):
-        self.pairs = {'type' : factType,'typeGroup' : factTypeGroup}
-        self.other_keys = []
-    
-    def add(self,key,value):
-        self.pairs.update({key:value})
-    
-    
-    def __str__(self, *args, **kwargs):
-        string = ""
-        for key in self.pairs.keys():
-            string += "\n%s: %s" %(key,self.pairs[key])
-        return string
-    
-    def toJson(self):
-        return json.dumps(self.pairs)
-    
+import Fact
     
 
 def openCalaisJsonToFacts(jsonObject,typeGroup):
